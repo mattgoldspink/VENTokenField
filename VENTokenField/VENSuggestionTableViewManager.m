e25958b268c3f6af8c8353e0cdd0fa8d596fb139
Matt Goldspink <mattgoldspink1@gmail.com>
2014-11-25 08:45:57 -0800

    Update VENSuggestionTableViewManager.m
    Making more generic to support a delegate supplying the UITableViewCell

--- VENTokenField/VENSuggestionTableViewManager.m
+++ VENTokenField/VENSuggestionTableViewManager.m
@@ -7,18 +7,16 @@
 //
 
 #import "VENSuggestionTableViewManager.h"
-#import "VENTokenField.h"
 
 @interface VENSuggestionTableViewManager ()
 
-@property (nonatomic, strong) NSArray *options;
+@property(nonatomic, strong) NSArray *options;
 
 @end
 
 @implementation VENSuggestionTableViewManager
 
-- (instancetype)initWithTokenField:(VENTokenField *)tokenField
-{
+- (instancetype)initWithTokenField:(VENTokenField *)tokenField {
     self = [super init];
     if (self) {
         self.tokenField = tokenField;
@@ -26,68 +24,52 @@
     return self;
 }
 
-- (NSString *)valueForIndexPath:(NSIndexPath *)indexPath
-{
-    return [self.dataSource tokenField:self.tokenField suggestionTitleForPartialText:self.tokenField.inputText atIndex:indexPath.row];
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return [self.delegate heightForCellAtIndex:indexPath.row];
 }
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
+- (UITableViewCell *)cellForIndexPath:(NSIndexPath *)indexPath {
+    return [self.dataSource tokenField:self.tokenField suggestionCellForPartialText:self.tokenField.inputText atIndex:indexPath.row];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return [self.dataSource tokenField:self.tokenField numberOfSuggestionsForPartialText:self.tokenField.inputText];
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"suggestionCell"];
-    
-    if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"suggestionCell"];
-    }
-    
-    cell.textLabel.text = [self valueForIndexPath:indexPath];
-    
-    return cell;
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return [self cellForIndexPath:indexPath];
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    [self.delegate suggestionManagerDidSelectValue:[self valueForIndexPath:indexPath] atIndex:indexPath.row];
-    
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self.delegate suggestionManagerDidSelectCell:[self cellForIndexPath:indexPath] atIndex:indexPath.row];
+
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
 }
 
-
-- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
-{
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
     [self.tokenField resignFirstResponder];
 }
 
-- (void)displayTableView
-{
+- (void)displayTableView {
     [self.tableView reloadData];
     [self.tokenField.window addSubview:self.tableView];
 }
 
-- (void)hideTableView
-{
+- (void)hideTableView {
     [self.tableView removeFromSuperview];
 }
 
-- (UITableView *)tableView
-{
+- (UITableView *)tableView {
     CGRect globalTokenViewFrame = [self.tokenField convertRect:self.tokenField.bounds toView:self.tokenField.window];
-    CGRect newFrame = CGRectMake(CGRectGetMinX(globalTokenViewFrame),
-                                 CGRectGetMaxY(globalTokenViewFrame),
-                                 CGRectGetWidth(globalTokenViewFrame),
-                                 CGRectGetHeight(self.tokenField.window.frame) - CGRectGetMaxY(globalTokenViewFrame));
+    CGRect newFrame =
+        CGRectMake(CGRectGetMinX(globalTokenViewFrame), CGRectGetMaxY(globalTokenViewFrame), CGRectGetWidth(globalTokenViewFrame),
+                   CGRectGetHeight(self.tokenField.window.frame) - CGRectGetMaxY(globalTokenViewFrame));
     if (!_tableView) {
-        _tableView = [[UITableView alloc] initWithFrame:newFrame
-                                                  style:UITableViewStylePlain];
+        _tableView = [[UITableView alloc] initWithFrame:newFrame style:UITableViewStylePlain];
         _tableView.delegate = self;
         _tableView.dataSource = self;
     } else {
